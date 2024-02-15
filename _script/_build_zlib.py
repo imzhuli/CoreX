e4731585464f902d7c183913b62b8568463b9b49
ZhuLi <imzhuli@vip.qq.com>
2024-02-15 16:05:56 +0800

    libz static only

--- _script/_build_zlib.py
+++ _script/_build_zlib.py
@@ -12,6 +12,21 @@ unzipped_src_dir = f"{unzip_dir}/zlib-1.3.1"
 install_dir = f"{cwd}/_3rd_installed"
 
 
+def disable_installing_shared_lib():
+    cmakefile = f"{unzipped_src_dir}/CMakeLists.txt"
+    try:
+        with open(cmakefile, "r") as sources:
+            lines = sources.readlines()
+        with open(cmakefile, "w") as sources:
+            for line in lines:
+                sources.write(line.replace(
+                    'TARGETS zlib zlibstatic', 'TARGETS zlibstatic'))
+    except Exception as e:
+        print("disable_installing_shared_lib error: %s" % e)
+        return False
+    return True
+
+
 def build():
     try:
         file = tarfile.open(src_file)
@@ -19,12 +34,16 @@ def build():
     finally:
         file.close()
 
+    if not disable_installing_shared_lib():
+        return False
+
     try:
         os.chdir(unzipped_src_dir)
         os.system(
             'cmake '
             '-Wno-dev '
             '-DBUILD_SHARED_LIBS=OFF '
+            '-DZLIB_BUILD_EXAMPLES=OFF '
             f'-DCMAKE_INSTALL_PREFIX={install_dir!r} -B build . ')
         os.system(f"cmake --build build -- all")
         os.system(f"cmake --build build -- install")
