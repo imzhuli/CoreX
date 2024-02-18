9ea6456d9ceaa5b82619b85b746c6255c1e641d8
ZhuLi <imzhuli@vip.qq.com>
2024-02-19 01:11:52 +0800

    fix cmake for rapidjson

--- _script/_build_rapidjson.py
+++ _script/_build_rapidjson.py
@@ -21,12 +21,32 @@ cpp_flags = """
 """.replace("\n", " ")
 
 
+def fix_cmake():
+    cmakefile = f"{unzipped_src_dir}/CMakeLists.txt"
+    find_target = """if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")"""
+    replace_target = """    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-class-memaccess")\n"""
+    try:
+        with open(cmakefile, "r") as sources:
+            lines = sources.readlines()
+        with open(cmakefile, "w") as sources:
+            for line in lines:
+                sources.write(line)
+                if 0 == line.find(find_target):
+                    sources.write(replace_target)
+    except Exception as e:
+        return False
+    return True
+
+
 def build():
     try:
         file = tarfile.open(src_file)
         file.extractall(unzip_dir)
     finally:
         file.close()
+
+    if not fix_cmake():
+        return False
 
     try:
         os.chdir(unzipped_src_dir)
