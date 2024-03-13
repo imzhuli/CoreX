6105aae675f11e092f56de708c0fe222cd783b87
ZhuLi <imzhuli@vip.qq.com>
2024-03-13 14:11:07 +0800

    remove default param

--- src/server_arch/service.hpp
+++ src/server_arch/service.hpp
@@ -42,7 +42,7 @@ class xService
 	, xAbstract {
 public:
 	X_API_MEMBER bool Init(xIoContext * IoContextPtr, const xNetAddress & BindAddress, bool ReusePort = false);
-	X_API_MEMBER bool Init(xIoContext * IoContextPtr, const xNetAddress & BindAddress, size_t MaxConnectionId, bool ReusePort = false);
+	X_API_MEMBER bool Init(xIoContext * IoContextPtr, const xNetAddress & BindAddress, size_t MaxConnectionId, bool ReusePort);
 	X_API_MEMBER void Tick();
 	X_API_MEMBER void Tick(uint64_t UpdatedNowMS);
 	X_API_MEMBER void Clean();
