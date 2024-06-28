a116e5b94cce2eb41ff8938ccd45e3f2e801f7da
Zhu Li <imzhuli@vip.qq.com>
2024-06-29 00:42:07 +0800

    dependency fix

---
+++ src/core/timer_wheel.cpp
@@ -0,0 +1,5 @@
+#include "./timer_wheel.hpp"
+
+X_BEGIN
+
+X_END
---
+++ src/core/timer_wheel.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include "./core_min.hpp"
+#include "./list.hpp"
+
+X_BEGIN
+
+class xTimerWheelNode;
+class xTimerWheel;
+
+class xTimerWheelNode {
+    friend class xTimerWheel;
+
+};
+
+class xTimerWheel {
+
+};
+
+X_END
