b31c90cd1dd90206c6859c8bd2de601a499c4a24
Zhu Li <imzhuli@vip.qq.com>
2024-07-15 04:28:31 +0800

    lean allocator call

--- src/core/memory.hpp
+++ src/core/memory.hpp
@@ -70,7 +70,7 @@ namespace __detail__ {
 }  // namespace __detail__
 
 template <typename T>
-inline constexpr size_t AllocAlignSize = __detail__::__AllocAlignSize__<T>::value;
+inline constexpr size_t AllocAlignSize = ::xel::__detail__::__AllocAlignSize__<T>::value;
 
 class xAllocator : xAbstract {
 public:
@@ -86,152 +86,167 @@ public:
 	 * @brief Free Allocated memory, null pointer should be accepted
 	 */
 	X_API_MEMBER virtual void Free(void * vpObject) noexcept;
-
-	X_INLINE void Free(const void * vpObject) noexcept {
-		Free(const_cast<void *>(vpObject));
-	}
-
-	template <typename T>
-	X_INLINE T * Create() {
-		auto p = this->Alloc(sizeof(T), AllocAlignSize<T>);
-		if (X_UNLIKELY(!p)) {
-			return nullptr;
-		}
-		try {
-			new (p) T;
-		} catch (...) {
-			this->Free(p);
-			throw;
-		}
-		return (T *)p;
-	}
-
-	template <typename T, typename... Args>
-	X_INLINE T * CreateValue(Args &&... args) {
-		auto p = this->Alloc(sizeof(T), AllocAlignSize<T>);
-		if (X_UNLIKELY(!p)) {
-			return nullptr;
-		}
-		try {
-			new (p) T(std::forward<Args>(args)...);
-		} catch (...) {
-			this->Free(p);
-			throw;
-		}
-		return (T *)p;
-	}
-
-	template <typename T, typename... Args>
-	X_INLINE T * CreateValueWithList(Args &&... args) {
-		auto p = this->Alloc(sizeof(T), AllocAlignSize<T>);
-		if (X_UNLIKELY(!p)) {
-			return nullptr;
-		}
-		try {
-			new (p) T{ std::forward<Args>(args)... };
-		} catch (...) {
-			this->Free(p);
-			throw;
-		}
-		return (T *)p;
-	}
-
-	template <typename T>
-	X_INLINE T * AlignedCreate(size_t vxAlignment) {
-		auto p = this->Alloc(sizeof(T), vxAlignment);
-		if (X_UNLIKELY(!p)) {
-			return nullptr;
-		}
-		try {
-			new (p) T;
-		} catch (...) {
-			this->Free(p);
-			throw;
-		}
-		return (T *)p;
-	}
-
-	template <typename T, typename... Args>
-	X_INLINE T * AlignedCreateValue(size_t vxAlignment, Args &&... args) {
-		auto p = this->Alloc(sizeof(T), vxAlignment);
-		if (X_UNLIKELY(!p)) {
-			return nullptr;
-		}
-		try {
-			new (p) T(std::forward<Args>(args)...);
-		} catch (...) {
-			this->Free(p);
-			throw;
-		}
-		return (T *)p;
-	}
-
-	template <typename T, typename... Args>
-	X_INLINE T * AlignedCreateValueWithList(size_t vxAlignment, Args &&... args) {
-		auto p = this->Alloc(sizeof(T), vxAlignment);
-		if (X_UNLIKELY(!p)) {
-			return nullptr;
-		}
-		try {
-			new (p) T{ std::forward<Args>(args)... };
-		} catch (...) {
-			this->Free(p);
-			throw;
-		}
-		return (T *)p;
-	}
-
-	template <typename T>
-	X_INLINE T * CreateArray(size_t n) {
-		auto p = this->Alloc(sizeof(T) * n, AllocAlignSize<T>);
-		if (X_UNLIKELY(!p)) {
-			return nullptr;
-		}
-		if constexpr (!std::is_trivially_constructible_v<T>) {
-			try {
-				new (p) T[n];
-			} catch (...) {
-				this->Free(p);
-				throw;
-			}
-		}
-		return (T *)p;
-	}
-
-	template <typename T, typename... Args>
-	X_INLINE T * CreateValueArray(size_t n, Args &&... args) {
-		auto p = this->Alloc(sizeof(T) * n, AllocAlignSize<T>);
-		if (X_UNLIKELY(!p)) {
-			return nullptr;
-		}
-		try {
-			new (p) T[n]{ std::forward<Args>(args)... };
-		} catch (...) {
-			this->Free(p);
-			throw;
-		}
-		return (T *)p;
-	}
-
-	template <typename T>
-	X_INLINE void Destroy(T * pObject) {
-		assert(pObject);
-		pObject->~T();
-		this->Free(pObject);
-	}
-
-	template <typename T>
-	X_INLINE void DestroyArray(T * pStart, size_t n) {
-		assert(pStart && n);
-		if constexpr (!std::is_trivially_destructible_v<T>) {
-			T * pObject = pStart;
-			for (size_t i = 0; i < n; ++i) {
-				pObject->~T();
-				++pObject;
-			}
-		}
-		this->Free(pStart);
-	}
 };
+
+namespace __detail__ {
+	X_INLINE void * Allocate(xAllocator * Allocator, size_t Size, size_t AlignedSize) {
+		if (Allocator) {
+			return Allocator->Alloc(Size, AlignedSize);
+		}
+		return xAllocator().Alloc(Size, AlignedSize);
+	}
+	X_INLINE void Free(xAllocator * Allocator, void * vpObject) {
+		if (Allocator) {
+			return Allocator->Free(vpObject);
+		}
+		return xAllocator().Free(vpObject);
+	}
+	X_INLINE void Free(xAllocator * Allocator, const void * vpObject) {
+		Free(Allocator, const_cast<void *>(vpObject));
+	}
+
+}  // namespace __detail__
+
+template <typename T>
+X_INLINE T * Create(xAllocator * Allocator) {
+	auto p = ::xel::__detail__::Allocate(Allocator, sizeof(T), AllocAlignSize<T>);
+	if (X_UNLIKELY(!p)) {
+		return nullptr;
+	}
+	try {
+		new (p) T;
+	} catch (...) {
+		::xel::__detail__::Free(Allocator, p);
+		throw;
+	}
+	return (T *)p;
+}
+
+template <typename T, typename... Args>
+X_INLINE T * CreateValue(xAllocator * Allocator, Args &&... args) {
+	auto p = ::xel::__detail__::Allocate(Allocator, sizeof(T), AllocAlignSize<T>);
+	if (X_UNLIKELY(!p)) {
+		return nullptr;
+	}
+	try {
+		new (p) T(std::forward<Args>(args)...);
+	} catch (...) {
+		::xel::__detail__::Free(Allocator, p);
+		throw;
+	}
+	return (T *)p;
+}
+
+template <typename T, typename... Args>
+X_INLINE T * CreateValueWithList(xAllocator * Allocator, Args &&... args) {
+	auto p = ::xel::__detail__::Allocate(Allocator, sizeof(T), AllocAlignSize<T>);
+	if (X_UNLIKELY(!p)) {
+		return nullptr;
+	}
+	try {
+		new (p) T{ std::forward<Args>(args)... };
+	} catch (...) {
+		::xel::__detail__::Free(Allocator, p);
+		throw;
+	}
+	return (T *)p;
+}
+
+template <typename T>
+X_INLINE T * AlignedCreate(xAllocator * Allocator, size_t vxAlignment) {
+	auto p = ::xel::__detail__::Allocate(Allocator, sizeof(T), vxAlignment);
+	if (X_UNLIKELY(!p)) {
+		return nullptr;
+	}
+	try {
+		new (p) T;
+	} catch (...) {
+		::xel::__detail__::Free(Allocator, p);
+		throw;
+	}
+	return (T *)p;
+}
+
+template <typename T, typename... Args>
+X_INLINE T * AlignedCreateValue(xAllocator * Allocator, size_t vxAlignment, Args &&... args) {
+	auto p = ::xel::__detail__::Allocate(Allocator, sizeof(T), vxAlignment);
+	if (X_UNLIKELY(!p)) {
+		return nullptr;
+	}
+	try {
+		new (p) T(std::forward<Args>(args)...);
+	} catch (...) {
+		::xel::__detail__::Free(Allocator, p);
+		throw;
+	}
+	return (T *)p;
+}
+
+template <typename T, typename... Args>
+X_INLINE T * AlignedCreateValueWithList(xAllocator * Allocator, size_t vxAlignment, Args &&... args) {
+	auto p = ::xel::__detail__::Allocate(Allocator, sizeof(T), vxAlignment);
+	if (X_UNLIKELY(!p)) {
+		return nullptr;
+	}
+	try {
+		new (p) T{ std::forward<Args>(args)... };
+	} catch (...) {
+		::xel::__detail__::Free(Allocator, p);
+		throw;
+	}
+	return (T *)p;
+}
+
+template <typename T>
+X_INLINE T * CreateArray(xAllocator * Allocator, size_t n) {
+	auto p = ::xel::__detail__::Allocate(Allocator, sizeof(T) * n, AllocAlignSize<T>);
+	if (X_UNLIKELY(!p)) {
+		return nullptr;
+	}
+	if constexpr (!std::is_trivially_constructible_v<T>) {
+		try {
+			new (p) T[n];
+		} catch (...) {
+			::xel::__detail__::Free(Allocator, p);
+			throw;
+		}
+	}
+	return (T *)p;
+}
+
+template <typename T, typename... Args>
+X_INLINE T * CreateValueArray(xAllocator * Allocator, size_t n, Args &&... args) {
+	auto p = ::xel::__detail__::Allocate(Allocator, sizeof(T) * n, AllocAlignSize<T>);
+	if (X_UNLIKELY(!p)) {
+		return nullptr;
+	}
+	try {
+		new (p) T[n]{ std::forward<Args>(args)... };
+	} catch (...) {
+		::xel::__detail__::Free(Allocator, p);
+		throw;
+	}
+	return (T *)p;
+}
+
+template <typename T>
+X_INLINE void Destroy(xAllocator * Allocator, T * pObject) {
+	assert(pObject);
+	pObject->~T();
+	::xel::__detail__::Free(Allocator, pObject);
+}
+
+template <typename T>
+X_INLINE void DestroyArray(xAllocator * Allocator, T * pStart, size_t n) {
+	assert(pStart && n);
+	if constexpr (!std::is_trivially_destructible_v<T>) {
+		T * pObject = pStart;
+		for (size_t i = 0; i < n; ++i) {
+			pObject->~T();
+			++pObject;
+		}
+	}
+	::xel::__detail__::Free(Allocator, pStart);
+}
 
 X_END
