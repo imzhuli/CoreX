eb2fc5a4148cba107475e97db198e247192deb75
xe.lee <imzhuli@gmail.com>
2024-07-24 09:06:01 +0800

    disable runasguar on windows

--- src/core/core_os._windows.cpp
+++ src/core/core_os._windows.cpp
@@ -5,6 +5,8 @@
 #define WINDOWS_LEAN_AND_MEAN
 #include <windows.h>
 
+X_BEGIN
+
 void Daemonize() {
 	// Step 1: Create a new process to run the daemonized code
 	STARTUPINFO         si;
@@ -43,42 +45,10 @@ void Daemonize() {
 	ExitProcess(0);
 }
 
-static PROCESS_INFORMATION CreateSubProcess() {
-
-	// Step 1: Create a new process to run the daemonized code
-	STARTUPINFO         si;
-	PROCESS_INFORMATION pi;
-
-	ZeroMemory(&si, sizeof(si));
-	si.cb          = sizeof(si);
-	si.dwFlags     = STARTF_USESTDHANDLES | STARTF_USESHOWWINDOW;
-	si.wShowWindow = SW_HIDE;
-
-	ZeroMemory(&pi, sizeof(pi));
-
-	// Create the new process
-	if (!CreateProcess(
-			NULL,              // No module name (use command line)
-			GetCommandLine(),  // Command line
-			NULL,              // Process handle not inheritable
-			NULL,              // Thread handle not inheritable
-			TRUE,              // Set handle inheritance to TRUE
-			CREATE_NO_WINDOW,  // No console window
-			NULL,              // Use parent's environment block
-			NULL,              // Use parent's starting directory
-			&si,               // Pointer to STARTUPINFO structure
-			&pi
-		)  // Pointer to PROCESS_INFORMATION structure
-	) {
-		X_PFATAL("CreateProcess failed: %i", GetLastError());
-		return;
-	}
-
-	return pi;
-}
-
 void RunAsGuard() {
 	X_PFATAL("RunAsGuard is not supported on windows platform");
 }
+
+X_END
 
 #endif
