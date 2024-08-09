667c7fe88f1a49d32ab346a2e21c24f295669fef
Zhu Li <imzhuli@vip.qq.com>
2024-08-09 16:23:03 +0800

    nothing

--- src_app/abort/main.cpp
+++ src_app/abort/main.cpp
@@ -1,6 +1,8 @@
 #include <core/core_min.hpp>
 using namespace xel;
 
+/* this program is for testing platform behaviour on QuickExit (coredump or not, wait for thread termination and extra..)*/
+
 int main(int, char **) {
 	Fatal("Abort");
 	return 0;
