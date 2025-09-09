a9f3254eff7292e0ffd247af6c73d59ebe7ecddd
imzhuli <imzhuli@vip.qq.com>
2025-09-09 09:50:50 +0800

    restricted xNoop type cast

--- src/core/core_noop.hpp
+++ src/core/core_noop.hpp
@@ -7,36 +7,33 @@ X_BEGIN
 
 template <auto... V>
 class xNoop;
+
 template <>
 class xNoop<> {
 private:
 	template <typename... Args>
-	using F = void (*)(Args...);
+	using FF = std::function<void(Args...)>;
 
 public:
 	template <typename... Args>
-	operator F<Args...>() const {
+	operator FF<Args...>() const {
 		return [](Args...) {};
 	}
 };
+
 template <auto V>
 class xNoop<V> {
 protected:
-	template <typename... Args>
-	using F = decltype(V) (*)(Args...);
 	template <typename... Args>
 	using FF = std::function<decltype(V)(Args...)>;
 
 public:
 	template <typename... Args>
-	operator F<Args...>() const {
+	operator FF<Args...>() const {
 		return [](Args...) { return V; };
 	}
-	template <typename... Args>
-	operator FF<Args...>() const {
-		return typename xNoop<V>::F<Args...>(*this);
-	}
 };
+
 template <auto... V>
 constexpr const auto Noop = xNoop<V...>();
 
