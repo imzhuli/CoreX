a5fb0859bdfcba70bae7c31ea38ad5b2cf090669
imzhuli <imzhuli@vip.qq.com>
2025-09-17 10:22:21 +0800

    move xNoop to namespace __detail__

--- src/core/functional.hpp
+++ src/core/functional.hpp
@@ -5,36 +5,40 @@
 
 X_BEGIN
 
+namespace __detail__ {
+
+	template <auto... V>
+	class xNoop;
+
+	template <>
+	class xNoop<> {
+	private:
+		template <typename... Args>
+		using FF = std::function<void(Args...)>;
+
+	public:
+		template <typename... Args>
+		operator FF<Args...>() const {
+			return [](Args...) {};
+		}
+	};
+
+	template <auto V>
+	class xNoop<V> {
+	protected:
+		template <typename... Args>
+		using FF = std::function<decltype(V)(Args...)>;
+
+	public:
+		template <typename... Args>
+		operator FF<Args...>() const {
+			return [](Args...) { return V; };
+		}
+	};
+
+}  // namespace __detail__
+
 template <auto... V>
-class xNoop;
-
-template <>
-class xNoop<> {
-private:
-	template <typename... Args>
-	using FF = std::function<void(Args...)>;
-
-public:
-	template <typename... Args>
-	operator FF<Args...>() const {
-		return [](Args...) {};
-	}
-};
-
-template <auto V>
-class xNoop<V> {
-protected:
-	template <typename... Args>
-	using FF = std::function<decltype(V)(Args...)>;
-
-public:
-	template <typename... Args>
-	operator FF<Args...>() const {
-		return [](Args...) { return V; };
-	}
-};
-
-template <auto... V>
-constexpr const auto Noop = xNoop<V...>();
+constexpr const auto Noop = __detail__::xNoop<V...>();
 
 X_END
